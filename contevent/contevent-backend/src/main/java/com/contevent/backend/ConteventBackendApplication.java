6ca7fe7b93b84b3f3e5a280b01ff9b10eeeb45cc
Fenyves96 <fenyves96@gmail.com>
2026-04-22 22:28:15 +0200

    init frontend

---
+++ contevent/contevent-backend/src/main/java/com/contevent/backend/ConteventBackendApplication.java
@@ -0,0 +1,12 @@
+package com.contevent.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConteventBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConteventBackendApplication.class, args);
+    }
+}
---
+++ contevent/contevent-backend/src/test/java/com/contevent/backend/ConteventBackendApplicationTests.java
@@ -0,0 +1,12 @@
+package com.contevent.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConteventBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
